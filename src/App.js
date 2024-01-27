aab5d9f2f3239106b45b002aeb700ebdff4d7f1f
kshitjj <kshitijrajgude@gmail.com>
2024-01-27 21:55:58 +0530

    fix "can't resolve 'Table.jsx'"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { Suspense } from "react";
-import { Canvas } from "react-three-fiber";
-import { OrbitControls } from "@react-three/drei";
-import { Model } from './Table.jsx'
-import { Sala } from './Sala.jsx'
 
 export default function App() {
   return (
