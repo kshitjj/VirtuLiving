55ad00dea03c82496e9042662a0c8c1806b85e47
kshitjj <kshitijrajgude@gmail.com>
2024-01-27 21:52:52 +0530

    remove react-three version

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import { Sala } from './Sala.jsx'
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas style={{ background: "#171717", width: "100%", height: "40%" }}>
-        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-        <ambientLight intensity={1} />
-        <Suspense fallback={null}>
-          <Sala />
-        </Suspense>
-        <OrbitControls
-          autoRotate={true}
-          enablePan={false}
-          enableZoom={true}
-          enableRotate={true}
-          minDistance={0.1}
-          maxDistance={2}
-          rotateSpeed={0.1}
-          zoomSpeed={1.2}
-          panSpeed={1}
-          position={[0, 0, 0]}   // Adjust the initial position here
-          target={[0, 0, 0]}   
-        />
-      </Canvas>
-      <model-viewer id="reveal" loading="eager" camera-controls touch-action="pan-y" auto-rotate src="table.glb" shadow-intensity="1" alt="A 3D model of a shishkebab"></model-viewer>
+      <model-viewer style={{ width: "50vw", height: "100vh" }} id="reveal" loading="eager" camera-controls touch-action="pan-y" auto-rotate src="Sala.glb" shadow-intensity="1" alt="A 3D model of a shishkebab"></model-viewer>
     </div>
   );
 }
