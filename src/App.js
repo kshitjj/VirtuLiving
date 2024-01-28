c4ec1d408ebf05a16158ea243f5fba3a1d114755
kshitjj <kshitijrajgude@gmail.com>
2024-01-28 20:29:05 +0530

    change css of modelviewer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Suspense } from "react";
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <model-viewer style={{ width: "50vw", height: "100vh" }} id="reveal" loading="eager" camera-controls touch-action="pan-y" auto-rotate src="Sala.glb" shadow-intensity="1" alt="A 3D model of a shishkebab"></model-viewer>
+      <model-viewer style={{ width: "100vw", height: "100vh" }} id="reveal" loading="eager" camera-controls touch-action="pan-y" auto-rotate src="Sala.glb" shadow-intensity="1" alt="A 3D model of a shishkebab"></model-viewer>
     </div>
   );
 }
